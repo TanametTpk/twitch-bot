c236cffeba12687e19d62f935ada30af5abc815a
tanamettpk <tanamet.tpk@gmail.com>
2021-05-01 17:31:28 +0700

    + dotenv config in index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 createConnection().then(async connection => {
     console.log("Inserting a new user into the database...");
