b280d02e821afb2db88b43a7a76feaea429adae4
tanamettpk <tanamet.tpk@gmail.com>
2021-05-30 20:18:16 +0700

    + equipment service test

--- src/interfaces/services/IEquipmentService.ts
+++ src/interfaces/services/IEquipmentService.ts
@@ -2,7 +2,7 @@ import { Equipment } from ".prisma/client";
 import { Character } from "@prisma/client";
 
 
-export default interface ICharacterEquipmentService {
+export default interface IEquipmentService {
     createEquipment(character: Character, atk: number, expired_time: number): Promise<Equipment | null>
     getEquipment(character_id: number): Promise<Equipment | null>
     removeEquipment(id: number): void;
