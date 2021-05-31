0f28b7bf347a450114273de693dfd243b3e02763
tanamettpk <tanamet.tpk@gmail.com>
2021-05-31 15:15:17 +0700

    + implement twitch command

--- src/bot/commands/twitch/attackBoss.ts
+++ src/bot/commands/twitch/attackBoss.ts
@@ -1,20 +1,42 @@
 import { Client, ChatUserstate } from "tmi.js";
 import ICommand from "../../../interfaces/ICommand";
 import ITwitchCommand from "../../../interfaces/ITwitchCommand";
+import IGameService from "../../../interfaces/services/IGameService";
+import services from "../../services";
+import randomIntBetween from "../../utils/randomIntBetween";
 
 class AttackBossCommand implements ICommand, ITwitchCommand {
+    private randomWord(): string {
+        let attackWords = [
+            "กระโดดเข้าไปตบบอส",
+            "จับบอสกระแทกเข่า",
+            "รังแกบอส",
+            "เอาบอสจับกดน้ำ",
+            "โยนหินใส่บอส",
+            "กระทืบบอส",
+            "ยืนด่าบอส",
+            "ใช้ความหล่อโจมตีบอส"
+        ]
+
+        return attackWords[randomIntBetween(0, attackWords.length - 1)]
+    }
+
     match(text: string): boolean {
         return text === "!attack boss";
     }
 
     perform(client: Client, channel: string, tags: ChatUserstate, message: string): void {
-        throw new Error("Method not implemented.");
+        if (!tags["user-id"]) return;
+        let game: IGameService = services.game;
 
-        // get player
+        if (!game.getGameManager().bossManager.isBossHasSpawned()) {
+            client.say(channel, `@${tags.username} ใจเย็นหนุ่มบอสยังไม่เกิด`);
+            return;
+        }
 
-        // attack boss
+        game.attackBossBy(tags["user-id"])
 
-        // return message
+        client.say(channel, `@${tags.username} ${this.randomWord()}`);
     }
 }
 
--- src/bot/commands/twitch/buyWeapon.ts
+++ src/bot/commands/twitch/buyWeapon.ts
@@ -1,16 +1,48 @@
 import { Client, ChatUserstate } from "tmi.js";
 import ICommand from "../../../interfaces/ICommand";
 import ITwitchCommand from "../../../interfaces/ITwitchCommand";
+import services from "../../services";
 
 class BuyWeaponCommand implements ICommand, ITwitchCommand {
     match(text: string): boolean {
-        return text === "!buy <int>";
+        return /!buy \d/.test(text);
     }
 
-    perform(client: Client, channel: string, tags: ChatUserstate, message: string): void {
-        // calculate damage
+    async perform(client: Client, channel: string, tags: ChatUserstate, message: string): Promise<void> {
+        if (!tags["user-id"]) return;
+        let character = await services.character.getCharacterByUserHash(tags["user-id"]);
+        if (!character) throw new Error("not found character")
 
-        // set item
+        let coin: number = Number(message.split(" ")[1])
+
+        if (coin < 0) {
+            client.say(channel, `
+                @${tags.username} เงินติดลบ คุณจะซื้อได้งายยยย ถามจริง!!!
+            `)
+            return
+        }
+
+        if (coin === 0) {
+            client.say(channel, `
+                @${tags.username} ของฟรีมันไม่มีในโลกโว้ยยยยย
+            `)
+            return
+        }
+
+        if (character.coin < coin) {
+            client.say(channel, `
+                @${tags.username} เงินไม่พอโว้ยยย ไปเรียนเลขมาใหม่ไป๊
+            `)
+            return
+        }
+        
+        await services.shop.buyEquipment(tags["user-id"], coin)
+        character = await services.character.getCharacterByUserHash(tags["user-id"]);
+        if (!character || !character.equipment) throw new Error("can't buy item")
+
+        let totalDmg: number = character.equipment.atk + character.atk;
+        let duration: number = character.equipment.expired_time;
+        client.say(channel, `@${tags.username}: ถอยดาบใหม่ได้แล้วโว้ย!! ตอนนี้ damage รวมกู ${totalDmg} เป็นเวลา ${duration} วัน`);
     }
 }
 
