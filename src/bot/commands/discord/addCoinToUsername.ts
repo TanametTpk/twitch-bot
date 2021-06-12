1a9db6db3c3a855b26a4db3f81b2753b63f3c3de
tanamettpk <tanamet.tpk@gmail.com>
2021-06-12 20:23:54 +0700

    + add and remove coin command to discord

---
+++ src/bot/commands/discord/addCoinToUsername.ts
@@ -0,0 +1,24 @@
+import { Message } from "discord.js";
+import ICommand from "../../../interfaces/ICommand";
+import IDiscordCommand from "../../../interfaces/IDiscordCommand";
+import IGameService from "../../../interfaces/services/IGameService";
+import services from "../../services";
+
+class AddCoinToUsernameCommand implements ICommand, IDiscordCommand {
+    match(text: string): boolean {
+        return /!give \d+ coin to [^ ]+/.test(text);
+    }
+
+    async perform(msg: Message) {
+        let coin = Number(msg.content.split(" ")[1]);
+        let username = msg.content.split(" ")[4]
+        let character = await services.character.getCharacterByName(username);
+
+        if (!character) return;
+
+        services.character.addCoinToCharacter(character.id, coin)
+        msg.channel.send(`ทำการเพิ่ม ${coin} coin ให้กับ ${username} แล้ว`);
+    }
+}
+
+export default new AddCoinToUsernameCommand();
