ea0742c749686ca32deac05339288fd3395653a9
tanamettpk <tanamet.tpk@gmail.com>
2021-06-12 20:09:12 +0700

    / ui

---
+++ src/bot/commands/discord/addOnlineDmg.ts
@@ -0,0 +1,24 @@
+import { Message } from "discord.js";
+import ICommand from "../../../interfaces/ICommand";
+import IDiscordCommand from "../../../interfaces/IDiscordCommand";
+import IGameService from "../../../interfaces/services/IGameService";
+import services from "../../services";
+
+class AddOnlineDmg implements ICommand, IDiscordCommand {
+    match(text: string): boolean {
+        return /!player add-dmg [^ ]+/.test(text);
+    }
+
+    async perform(msg: Message) {
+        let game: IGameService = services.game;
+        let playerId = msg.content.split(" ")[2];
+        let user = await services.user.getUserByHash(playerId)
+
+        if (!user) return;
+
+        game.getGameManager().playerManager.addOnlinePlayer(user)
+        msg.channel.send("เพิ่ม dmg ให้แล้ว");
+    }
+}
+
+export default new AddOnlineDmg();
