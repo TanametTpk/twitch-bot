3cbb00bc6a1ea0e50d0e3feb35eda7c42853877c
tanamettpk <tanamet.tpk@gmail.com>
2021-06-13 18:51:39 +0700

    + more test from cheer reward

--- src/bot/services/__test__/UserServiceTest.spec.ts
+++ src/bot/services/__test__/UserServiceTest.spec.ts
@@ -79,3 +79,71 @@ test('should return null when hash not exist', async() => {
     let targetUser = await service.getUserByHash("hash1")
     expect(targetUser).toEqual(null)
 })
+
+test('should add cheer to user', async() => {
+    const user: User = { 
+        id: 1,
+        name: "user1",
+        hash: "hash1",
+        cheer: 10
+    }
+
+    await prismaMock.user.update.mockResolvedValue(user)
+
+    let targetUser = await service.addCheerReward(1, 10)
+    expect(targetUser).toEqual(user)
+})
+
+test('should return null when add cheer with negative number', async() => {
+    let targetUser = await service.addCheerReward(1, -5)
+    expect(targetUser).toEqual(null)
+})
+
+test('should remove cheer from user', async() => {
+    const user: User = { 
+        id: 1,
+        name: "user1",
+        hash: "hash1",
+        cheer: 10
+    }
+
+    const expectedUser: User = { 
+        id: 1,
+        name: "user1",
+        hash: "hash1",
+        cheer: 0
+    }
+
+    await prismaMock.user.findFirst.mockResolvedValue(user)
+    await prismaMock.user.update.mockResolvedValue(expectedUser)
+
+    let targetUser = await service.removeCheerReward(1, 10)
+    expect(targetUser).toEqual(expectedUser)
+})
+
+test('should remove all cheer from user when reward is more than user cheer', async() => {
+    const user: User = { 
+        id: 1,
+        name: "user1",
+        hash: "hash1",
+        cheer: 10
+    }
+
+    const expectedUser: User = { 
+        id: 1,
+        name: "user1",
+        hash: "hash1",
+        cheer: 0
+    }
+
+    await prismaMock.user.findFirst.mockResolvedValue(user)
+    await prismaMock.user.update.mockResolvedValue(expectedUser)
+
+    let targetUser = await service.removeCheerReward(1, 100)
+    expect(targetUser).toEqual(expectedUser)
+})
+
+test('should return null when remove cheer with negative number', async() => {
+    let targetUser = await service.addCheerReward(1, -5)
+    expect(targetUser).toEqual(null)
+})
