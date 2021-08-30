bea7d9ee2fe9422c3c6257b02e96b6d84199b829
tanamettpk <tanamet.tpk@gmail.com>
2021-08-30 15:56:25 +0700

    + pubsub redemtion event

--- src/bot/publishers/TwitchPublisher.ts
+++ src/bot/publishers/TwitchPublisher.ts
@@ -6,15 +6,16 @@ import IChannelPointAction from "../../interfaces/IChannelPointAction";
 import IMiddleware from "../../interfaces/IMiddleware";
 import ITwitchSubscriptionStategy from "../../interfaces/ITwitchSubscriptionStategy";
 import ITwitchCheerStategy from "../../interfaces/ITwitchCheerStategy";
-import ITwitchMysteryGift from "../../interfaces/ITwitchMysteryGift";
-import { PubSubClient } from "twitch-pubsub-client/lib";
-import { AccessToken, StaticAuthProvider } from "twitch-auth/lib";
-import { ApiClient } from "twitch/lib";
+import { PubSubClient, PubSubListener } from "twitch-pubsub-client";
+import { StaticAuthProvider } from "twitch-auth";
+import { ApiClient } from "twitch";
 
 export default class TwitchCommander extends AbstractPublisher<ITwitchCommand> {
     private client: tmi.Client
     private pubSubClient: PubSubClient = new PubSubClient();
     private userId: any;
+    private listener: PubSubListener<never> | undefined;
+    private apiClient: ApiClient;
 
     public constructor(
         commands: ITwitchCommand[],
@@ -28,73 +29,16 @@ export default class TwitchCommander extends AbstractPublisher<ITwitchCommand> {
         let clientId = process.env.TWITCH_CLIENT_ID || ""
         let accessToken = process.env.TWITCH_CLIENT_ACCESS_TOKEN || ""
 
+        console.log(clientId, accessToken);
+        
         let authProvider = new StaticAuthProvider(clientId, accessToken)
-        let apiClient = new ApiClient({ authProvider })
-        this.pubSubClient.registerUserListener(apiClient).then(userId => this.userId = userId)
+        this.apiClient = new ApiClient({ authProvider })
     }
 
     public start(): void {
         this.client.connect();
 
-        this.pubSubClient.onRedemption({id: this.userId}, async (msg) => {
-            let channel = process.env.tmi_channel_name as string
-            // *THIS IS FAKE STATE FOR REUSE FROM TMIJS*
-            let tags: ChatUserstate = {
-                badges: {},
-                color: "",
-                'display-name': msg.userDisplayName,
-                'emotes': {},
-                'mod': false,
-                'room-id': '',
-                'subscriber': false,
-                'turbo': false,
-                'user-id': msg.userId,
-                'user-type': '',
-                'emotes-raw': '',
-                "badges-raw": '',
-                'username': msg.userName,
-                'message-type': 'action'
-            } as ChatUserstate
-            let message: string = msg.message
-
-            let middlewareTasks = this.middlewareActions.map(
-                async (middleware: IMiddleware) => 
-                    middleware.perform(
-                        this.client,
-                        channel,
-                        tags,
-                        message
-                    )
-            )
-
-            await Promise.all(middlewareTasks)
-
-            if (tags["custom-reward-id"]){
-                if (process.env.DEBUG_CUSTOM_REWARD) {
-                    console.log(tags["custom-reward-id"]);
-                }
-
-                let rewardActionTasks = this.rewardActions.map(
-                    async (action) => {
-                        if (action.match(tags["custom-reward-id"]))
-                            action.perform(this.client, channel, tags, message);
-                    }
-                )
-
-                await Promise.all(rewardActionTasks)
-            }
-
-            let isCommandAvailable = process.env.AVALABLE_COMMAND === "true"
-            if (isCommandAvailable) {
-                const command = this.findMatchCommand(message);
-                if (command) command.perform(
-                    this.client,
-                    channel,
-                    tags,
-                    message
-                )
-            }
-        })
+        this.registerPubSub();
 
         this.client.on('subscription', (channel: string, username: string, _, message: string, userstate: tmi.SubUserstate) => {
             this.subscriptionStategy.perform(this.client, channel, message, username, userstate);
@@ -151,7 +95,75 @@ export default class TwitchCommander extends AbstractPublisher<ITwitchCommand> {
         });
     }
 
+    private async registerPubSub() {
+        this.userId = await this.pubSubClient.registerUserListener(this.apiClient)
+
+        this.listener = await this.pubSubClient.onRedemption({id: this.userId}, async (msg) => {
+            let channel = process.env.tmi_channel_name as string
+            // *THIS IS FAKE STATE FOR REUSE FROM TMIJS*
+            let tags: ChatUserstate = {
+                badges: {},
+                color: "",
+                'display-name': msg.userDisplayName,
+                'emotes': {},
+                'mod': false,
+                'room-id': '',
+                'subscriber': false,
+                'turbo': false,
+                'user-id': msg.userId,
+                'user-type': '',
+                'emotes-raw': '',
+                "badges-raw": '',
+                'username': msg.userName,
+                'message-type': 'action',
+                'custom-reward-id': msg.rewardId
+            } as ChatUserstate
+            let message: string = msg.message
+            
+            let middlewareTasks = this.middlewareActions.map(
+                async (middleware: IMiddleware) => 
+                    middleware.perform(
+                        this.client,
+                        channel,
+                        tags,
+                        message
+                    )
+            )
+
+            await Promise.all(middlewareTasks)
+
+            if (tags["custom-reward-id"]){
+                if (process.env.DEBUG_CUSTOM_REWARD) {
+                    console.log(tags["custom-reward-id"]);
+                }
+
+                let rewardActionTasks = this.rewardActions.map(
+                    async (action) => {
+                        if (action.match(tags["custom-reward-id"]))
+                            action.perform(this.client, channel, tags, message);
+                    }
+                )
+
+                await Promise.all(rewardActionTasks)
+            }
+
+            let isCommandAvailable = process.env.AVALABLE_COMMAND === "true"
+            if (isCommandAvailable) {
+                const command = this.findMatchCommand(message);
+                if (command) command.perform(
+                    this.client,
+                    channel,
+                    tags,
+                    message
+                )
+            }
+        })
+
+        console.log("Finish Listen to PubSub");
+    }
+
     public stop(): void {
         this.client.disconnect()
+        this.listener?.remove()
     }
 }
